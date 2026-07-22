751941f807668145ec053ca70f9a051bfad7d289
MrKroger <ked2007kudelkin@gmail.com>
2026-07-22 03:24:07 +0300

    Доп удобства

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
 import type { NextConfig } from "next";
 
+// Дефолтные значения на случай, если переменная в .env не задана
+const defaultOrigins = ["localhost:3000", "*.local"];
+
+// Считываем переменную окружения и разбиваем её по запятой
+const envOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
+  : defaultOrigins;
+
 const nextConfig: NextConfig = {
-    experimental: {
-        serverActions:{
-            allowedOrigins:[
-                "localhost:3000",
-                "*.local",
-                "172.26.48.1"
-            ],
-            bodySizeLimit: "8mb",
-        }
-    }
+  experimental: {
+    serverActions: {
+      allowedOrigins: envOrigins,
+      bodySizeLimit: "8mb",
+    },
+  },
 };
 
 export default nextConfig;
