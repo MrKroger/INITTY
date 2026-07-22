a8c5349b4b4f52c67e77cab5e62a4bfd09dd824c
MrKroger <ked2007kudelkin@gmail.com>
2026-07-22 03:25:35 +0300

    Вот теперь удобства

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-// Дефолтные значения на случай, если переменная в .env не задана
 const defaultOrigins = ["localhost:3000", "*.local"];
 
-// Считываем переменную окружения и разбиваем её по запятой
 const envOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
   : defaultOrigins;
